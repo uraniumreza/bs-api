6a8ae4447d777c36b8f6241945f30199474903c7
Nayeem Reza <reza.uranium@gmail.com>
2019-03-09 23:34:27 +0600

    Update product model schema and add method, statics

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,39 +1,83 @@
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: {
-    type: String,
-    required: true,
-    trim: true,
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    brand: {
+      type: String,
+      default: 'BS Trading',
+      maxlength: 64,
+    },
+    category: {
+      type: String,
+      enum: ['Cosmetics', 'Electronics', 'Accessories'],
+      required: true,
+      index: true,
+    },
+    colors: {
+      type: [String],
+      default: undefined,
+    },
+    sizes: {
+      type: [String],
+      default: undefined,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    discount: {
+      type: Number,
+      default: 0,
+    },
+    images: {
+      type: [String],
+      default: undefined,
+      required: true,
+    },
+    description: {
+      type: String,
+      maxlength: 1500,
+    },
+    stock_count: {
+      type: Number,
+      min: [0, 'Out of Stock'],
+      required: true,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
   },
-  brand: String,
-  category: {
-    type: String,
-    enum: ['Cosmetics', 'Electronics', 'Accessories'],
-    required: true,
-    index: true,
+  {
+    timestamps: true,
   },
-  colors: {
-    type: [String],
-    default: undefined,
+);
+
+productSchema.method({
+  transformList() {
+    const transformed = {};
+    const fields = ['id', 'name', 'price', 'discount', 'stock_count', 'active'];
+
+    fields.forEach((field) => {
+      transformed[field] = this[field];
+    });
+
+    return transformed;
   },
-  sizes: {
-    type: [String],
-    default: undefined,
-  },
-  price: Number,
-  discount: Number,
-  images: {
-    type: [String],
-    default: undefined,
-  },
-  description: String,
-  stock_count: {
-    type: Number,
-    min: [0, 'Out of Stock'],
-  },
-  active: Boolean,
 });
+
+productSchema.statics = {
+  list(options) {
+    return this.find(options)
+      .sort({ createdAt: -1 })
+      .exec();
+  },
+};
 
 module.exports = mongoose.model('Product', productSchema);
