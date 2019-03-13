af81a4b6949815f7d533ccdd283564d98c270a7d
Nayeem Reza <reza.uranium@gmail.com>
2019-03-13 22:48:55 +0600

    :star: protect product routes

--- src/routes/v1/product.route.js
+++ src/routes/v1/product.route.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const controller = require('../../controllers/product.controller');
+const { authorize, ADMIN, LOGGED_USER } = require('../../middlewares/auth');
 
 const router = express.Router();
 
 router
   .route('/')
-  .get(controller.list)
-  .post(controller.create);
+  .get(authorize(LOGGED_USER), controller.list)
+  .post(authorize(ADMIN), controller.create);
 
 router
   .route('/:productId')
-  .get(controller.get)
-  .patch(controller.update)
-  .delete(controller.remove);
+  .get(authorize(LOGGED_USER), controller.get)
+  .patch(authorize(ADMIN), controller.update)
+  .delete(authorize(ADMIN), controller.remove);
 
 module.exports = router;
