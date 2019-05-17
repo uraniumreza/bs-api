aefb754d90f91c110ad82dce768468a74783f25a
Nayeem Reza <reza.uranium@gmail.com>
2019-05-18 00:24:55 +0600

    update token expiry to 1 year

--- config/vars.js
+++ config/vars.js
@@ -1,7 +1,7 @@
 module.exports = {
   jwtSecret: process.env.JWT_SECRET || 'bst',
-  jwtExpirationInterval: process.env.JWT_EXPIRATION_MINUTES || 43200,
+  jwtExpirationInterval: process.env.JWT_EXPIRATION_MINUTES || 525600,
   CLOUDINARY_API_KEY: '851891542411789',
   CLOUDINARY_CLOUD_NAME: 'bs-trading',
-  CLOUDINARY_API_SECRET: 'Uxo89vhetIATZ0wyhCaBKgJBNHg'
+  CLOUDINARY_API_SECRET: 'Uxo89vhetIATZ0wyhCaBKgJBNHg',
 };
